7850bdd8caaf0b76b51d2fb2848e91cdfaad7189
VDABHEVERLEE\filip.hamakers <filiphamakers@hotmail.com>
2017-10-04 11:22:16 +0200

    Oefening 1.6 Adres p.5

--- WebContent/WEB-INF/JSP/index.jsp
+++ WebContent/WEB-INF/JSP/index.jsp
@@ -4,9 +4,11 @@
 <head>
 <title>Frituur Frida</title>
 <link rel="stylesheet" href="styles/default.css">
+<link rel="shortcut icon" href="images/favicon.ico">
 </head>
 <body>
 	<h1>Frituur Frida</h1>
+	<p>${adres.straat} ${adres.huisNr} ${adres.gemeente.naam} ${adres.gemeente.postcode}</p>
 	<p>Wij zijn vandaag ${openGesloten}</p>
 	<img src="images/${openGesloten}.png" alt="${openGesloten}">
 </body>
--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import be.vdab.entities.Adres;
+import be.vdab.entities.Gemeente;
+
 /**
  * Servlet implementation class IndexServlet
  */
@@ -27,6 +30,7 @@ public class IndexServlet extends HttpServlet {
 		DayOfWeek dag = LocalDateTime.now().getDayOfWeek();
 		request.setAttribute("openGesloten",
 				dag.equals(DayOfWeek.THURSDAY) || dag.equals(DayOfWeek.MONDAY) ? "gesloten" : "open");
+		request.setAttribute("adres", new Adres("Frikandelstraat","12",new Gemeente("Andalousegem",1234)));
 		request.getRequestDispatcher(VIEW).forward(request, response);
 
 	}
